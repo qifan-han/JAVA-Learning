bd87087071b8545a2f3ff51a22db2846d5871acc
Qifan Han <qh2175@columbia.edu>
2018-03-08 22:30:41 +0800

    Create Personal Library.java

---
+++ Personal Library.java
@@ -0,0 +1,47 @@
+//This program stores some books and their completion status in a HashMap.
+
+import java.util.HashMap;
+
+class Library {
+ 
+  public Library() {
+    
+  }
+  
+  public void getFinishedBooks(HashMap<String, Boolean> library) {
+
+    if (library.size() < 1) {
+      
+      System.out.println("Sorry, the HashMap is empty!");
+      
+    } else {
+      
+      for (String book : library.keySet()) {
+       
+        if (library.get(book) == true) {
+         
+          System.out.println(book);
+          
+        }
+        
+      }
+      
+    }
+    
+  }
+  
+  public static void main(String[]args) {
+   
+    HashMap<String, Boolean> myBooks = new HashMap<String, Boolean>();
+    myBooks.put("Road Down The Funnel", true);
+    myBooks.put("Rat: A Biology", false);
+    myBooks.put("TimeIn", true);
+    myBooks.put("3D Food Printing", false);
+    
+    Library myLibrary = new Library();
+    myLibrary.getFinishedBooks(myBooks);
+    
+    
+  }
+  
+}
